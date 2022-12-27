3ff88f74dceef1283332a2c2505ee94b9cca9841
David <davidrsmk144@gmail.com>
2022-12-27 18:47:40 -0500

    Deploy production

--- Backend/config.js
+++ Backend/config.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV != 'production'){
+// if(process.env.NODE_ENV != 'production'){
     require('dotenv').config()
-}
+// }
 
 module.exports = {
     URI_DB: process.env.URI_DB
